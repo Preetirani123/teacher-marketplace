2ac56d040eeffa82d7118e41c4416136fb3ea2fd
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-04-10 14:29:11 -0600

    added login functions

--- react-front-end/src/Components/Login/Login.js
+++ react-front-end/src/Components/Login/Login.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+import axios from "axios";
 import {TextField, Button, Typography} from '@material-ui/core';
 import useStyles from './styles';
+
+
 export default function Login() {
   const classes = useStyles();
 
+  const [loggedIn, setState] = useState(false)
+
+  function login() {
+    return axios.post('/login')
+    .then((res)=> {
+      console.log(res[0])
+    });
+  };
+
+  function autoLogin() {
+    return axios.get("/login/1").then((res) => {
+      console.log(res[0]);
+      setState((prevState) => ({
+        loggedIn: !prevState.loggedIn,
+      }));
+    });
+  };
+
   return (
     <div>
-        <form className={classes.root} noValidate autoComplete="off">
+        <form className={classes.root} noValidate autoComplete="off" onSubmit={(event) => event.preventDefault()}>
          <Typography variant = "h4" className={classes.spread}>
            Login
          </Typography>
@@ -18,8 +39,13 @@ export default function Login() {
             type="password" autoComplete="current-password" />
           </div>
           <div>
-            <Button variant="contained" color="primary" className = {classes.spread}>
+            <Button variant="contained" color="primary" className = {classes.spread} onSubmit={login}>
               Submit
+            </Button>
+          </div>
+          <div>
+            <Button variant="contained" color="primary" className = {classes.spread} onClick={autoLogin}>
+              Auto-Login
             </Button>
           </div>
         </form> 
