58c2330fdcd1a8d2920e5b0b0d27620f4bceb3a4
Vineet Pande <vsp4991@gmail.com>
2021-04-10 03:30:20 -0700

    added some changes to react router

--- react-front-end/src/Components/Main/Main.js
+++ react-front-end/src/Components/Main/Main.js
@@ -2,26 +2,26 @@ import React from 'react'
 import ProductContainer from '../ProductContainer/ProductContainer'
 import TopContent from '../TopContent/TopContent'
 import Aside from '../Aside/Aside'
+import Nav from '../Nav/Nav';
 import useStyles from './styles';
+import Footer from '../Footer/Footer';
+import {BrowserRouter as Router} from 'react-router-dom'
+
 
 export default function Main() {
   const classes = useStyles();
   return (
     <div>
+      <Router>
       <main >
-        <TopContent />
-        <div className={classes.main}>
-          <div className={classes.aside}>
-            <Aside />
-          </div>
-          <div className={classes.product}>
-            <ProductContainer />
-          </div>
+        <Nav />
+
+
         
-        </div>
         
+        <Footer />
       </main>
-      
+      </Router>
     </div>
   )
 }
