32bb9bd6d19da7168d2619ccd6cb14d24ecfef46
Vineet Pande <vsp4991@gmail.com>
2021-04-11 03:57:40 -0700

    modified logout route

--- express-back-end/routes/logout.js
+++ express-back-end/routes/logout.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-  router.get('/', (req, res) => {
-    res.clearCookie('session');
-    res.clearCookie('session.sig');
-    res.redirect('/');
+  //endpoint for allowing users to log out of their accounts
+  router.post('/logout', (req, res) => {
+      req.session = null;
+      res.redirect('/');
   });
   return router;
 };
