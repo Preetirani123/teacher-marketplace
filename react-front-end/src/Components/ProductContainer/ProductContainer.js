5a24045fb5d20f09f46bb281ef82ff250d294bf4
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-04-12 12:26:02 -0600

    Added basic functionality of pagination

--- react-front-end/src/Components/ProductContainer/ProductContainer.js
+++ react-front-end/src/Components/ProductContainer/ProductContainer.js
@@ -6,29 +6,66 @@ import Aside from '../Aside/Aside'
 import useStyles from './styles';
 import axios from "axios";
 
-
-
 export default function ProductContainer(props) {
  
   const [state, setState] = useState({
-    prod:[]
+    prod:[],
+    page: 1,
+    prodsPerPage: 30
   })
 
   useEffect(() => {
-    Promise.all([
-      axios.get('/product'),
-    ]).then((all) => {
+    Promise.all([axios.get("/product")]).then((all) => {
       console.log(all[0].data);
-      console.log("xpxpxpxpxpxpxpx")
+      console.log("xpxpxpxpxpxpxpx");
 
-      setState(prev => (
-        {
-          ...prev,
-          prod: all[0].data
-        }
-      ))
-    })
+      setState((prev) => ({
+        ...prev,
+        prod: all[0].data,
+      }));
+    });
   }, []);
+
+  const handleClick = (pageNum) => {
+    setState((prevState) => ({
+      ...prevState,
+      page: Number(pageNum),
+    }));
+  };
+
+  const { prod, page, prodsPerPage } = state;
+
+  // Logic for displaying prod
+  const indexOfLastProd = page * prodsPerPage;
+    const indexOfFirstProd = indexOfLastProd - prodsPerPage;
+    console.log(prod);
+    const currentProds = prod.slice(indexOfFirstProd, indexOfLastProd);
+
+    const renderProds = currentProds.map((prod, index) => {
+      return (
+        <Grid item key={prod.id} xs={12} sm={6} md={4} lg={3}>
+          <Product product={prod} setCart={props.setCart} />
+        </Grid>
+      );
+    });
+
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(prod.length / prodsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <li
+          key={number}
+          id={number}
+          onClick={(event) => handleClick(event.target.id)}
+        >
+          {number}
+        </li>
+      );
+    });
 
   const classes = useStyles();
   
@@ -36,15 +73,14 @@ export default function ProductContainer(props) {
     <div>
      <Aside />
      <Grid container justify="center" spacing={4} className = {classes.spread}>
-       
-       {state.prod.map((product) => (
+        {renderProds}
+       {/* {state.prod.map((product) => (
         <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
          <Product product = {product} setCart = {props.setCart} />
        </Grid>
-       ))}
-       
+       ))} */}
      </Grid>
-      
+      {renderPageNumbers}
     </div>
   )
 }
