3212ecad5e092346338cd227c9102ff3d630bed6
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-04-15 12:23:02 -0600

    Added Stripe Integration and Auto Pay Button

--- react-front-end/src/Components/Checkout/Checkout.js
+++ react-front-end/src/Components/Checkout/Checkout.js
@@ -1,44 +1,105 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Button } from '@material-ui/core';
+import { CardMedia, Icon, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@material-ui/core';
 import useStyles from './styles';
 import { useHistory, Link } from "react-router-dom";
 import emailjs from 'emailjs-com';
 import{ init } from 'emailjs-com';
+
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import './CheckoutStyles.scss';
+
+const promise = loadStripe("pk_test_51IamO7F2v7FmERryBHILYa3LMwxLz5T5K4xSOzE8zlXrhZEeKZJfznkXYWFSVEzWFDLq7tMwHJc4lDodRUGZBySF00H7oR9ZqR");
+
 const {REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, REACT_APP_USER_ID} = process.env
-
-
-
-
 
 export default function Checkout(props) {
   const history = useHistory();
   const classes = useStyles();
-  console.log(history)
-  console.log("%%%%%%%%%%%%%%%%%%%%")
-
+  console.log(history);
+  console.log("%%%%%%%%%%%%%%%%%%%%");
+  console.log(props);
 
   // function for email
-
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('REACT_APP_SERVICE_ID', 'REACT_APP_TEMPLATE_ID', e.target, 'REACT_APP_USER_ID')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "REACT_APP_SERVICE_ID",
+        "REACT_APP_TEMPLATE_ID",
+        e.target,
+        "REACT_APP_USER_ID"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   }
 
-  // Note please make the checkout form and pdf file name is Pdf and user name is name 
+  // Note please make the checkout form and pdf file name is Pdf and user name is name
   //and call the sendEmail funtion in checkout form like this onSubmit={sendEmail}
-  
-  
+
   return (
     <div>
-      <h1>Helllo</h1>
-      
+      <TableContainer
+        key={Math.random()}
+        component={Paper}
+        className={classes.cartContainer}
+        style={{
+          boxshadow:
+            "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
+        }}
+      >
+        <Table
+          key={Math.random()}
+          className={classes.table}
+          aria-label="simple table"
+        >
+          <TableHead color="primary">
+            <TableRow className={classes.tableHead}>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Quantity</TableCell>
+              <TableCell align="right">Price</TableCell>
+            </TableRow>
+          </TableHead>
+          {props.items.length === 0 ? (
+            <p>Cart is empty - Go to the main page and buy some things</p>
+          ) : (
+            <TableBody key={Math.random()}>
+              {props.items.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell align="right">{row.name}</TableCell>
+                  <TableCell align="right">{row.qty}</TableCell>
+                  <TableCell align="right">${row.price}</TableCell>
+                </TableRow>
+              ))}
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Total ${props.total}</TableCell>
+              </TableRow>
+            </TableBody>
+          )}
+        </Table>
+      </TableContainer>
+      <div className='stripe'>
+        <Elements stripe={promise}>
+          <CheckoutForm />
+        </Elements>
+        {/* BACKEND IS NOT INTEGRATED */}
+      </div>
+      <div className='AutoPay'>
+      <Button type = "submit" variant="contained" color="primary" className = {classes.spread}>
+            Demo Mode - AutoPay
+      </Button>
+      </div>
     </div>
-  )
+  );
 }
