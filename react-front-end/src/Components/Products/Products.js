8042e851ed5286e2bdc52ee003d926bbe94f5a6b
Vineet Pande <vsp4991@gmail.com>
2021-04-16 08:28:55 -0700

    fixed env issue

--- react-front-end/src/Components/Products/Products.js
+++ react-front-end/src/Components/Products/Products.js
@@ -6,52 +6,41 @@ import { useHistory, Link } from "react-router-dom";
 import Nav from '../Nav/Nav';
 import PhotoCamera from '@material-ui/icons/PhotoCamera';
 import { uploadFile } from 'react-s3';
-//require('dotenv').config();
 
 
+const {REACT_APP_S_ACCESS_KEY, REACT_APP_S_SECRET_KEY} = process.env
 
 const S3_BUCKET ='admin-teachers-marketplace';
 const REGION ='us-west-2';
-const {S3_ACCESS_KEY, S3_SECRET_KEY, REACT_APP_APIKEY} = process.env
-console.log(S3_ACCESS_KEY)
-console.log(S3_SECRET_KEY)
-console.log(REACT_APP_APIKEY)
 
 const config = {
     bucketName: S3_BUCKET,
     dirName: 'photos',
     region: REGION,
-    accessKeyId: S3_ACCESS_KEY,
-    secretAccessKey: S3_SECRET_KEY
+    accessKeyId: REACT_APP_S_ACCESS_KEY,
+    secretAccessKey: REACT_APP_S_SECRET_KEY
 }
 
 export default function Products(props) {
   const history = useHistory();
   const classes = useStyles();
-  const [selectedFile, setSelectedFile] = useState(null);
+  
 
-  const handleFileInput = (e) => {
-    setSelectedFile(e.target.files[0]);
-  }
-
-  const handleUpload = async (file) => {
-      uploadFile(file, config)
+  const handleUpload = async (e) => {
+      uploadFile(e.target.files[0], config)
           .then(data => console.log(data))
           .catch(err => console.error(err))
   }
 
  
-  
-  
   return (
     <div>
       <Nav count = {props.count} setEm = {props.setEm} />
 
+      {/* <input type="file" accept = "image/*" onChange={handleUpload}/> */}
       
-
-      <input type="file" onChange={handleFileInput}/>
-      Hell well
-      <button onClick={() => handleUpload(selectedFile)}> Upload to S3</button>
+      
+      
           
 
       
