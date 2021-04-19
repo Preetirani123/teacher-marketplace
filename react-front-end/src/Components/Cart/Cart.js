26c7da4c8f37eb9f79d34150b26d51f9042c3c1f
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-04-18 18:52:47 -0600

    fixed bug in checkout that would allow checkout with an empty cart

--- react-front-end/src/Components/Cart/Cart.js
+++ react-front-end/src/Components/Cart/Cart.js
@@ -1,90 +1,137 @@
 import React, { useState, useEffect } from "react";
-import { CardMedia, Icon, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@material-ui/core';
-import useStyles from './styles';
+import {
+  CardMedia,
+  Icon,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+} from "@material-ui/core";
+import useStyles from "./styles";
 import { useHistory, Link } from "react-router-dom";
-import Login from '../Login/Login';
-import Nav from '../Nav/Nav';
-import axios from 'axios'
-
-
-
-
+import Login from "../Login/Login";
+import Nav from "../Nav/Nav";
+import axios from "axios";
 
 export default function Cart(props) {
   const history = useHistory();
   const classes = useStyles();
 
-  function changeQ (sign, id) {
-     props.changeQty(sign, id)
+  function changeQ(sign, id) {
+    props.changeQty(sign, id);
   }
 
-
   return (
-    
+    <div>
+      <Nav count={props.count} setEm={props.setEm} setId={props.setId} />
 
-      <div>
-        <Nav count = {props.count} setEm = {props.setEm} setId = {props.setId} />
+      <div className={classes.cartwidth}>
+        <div className={classes.butts}>
+          <Button
+            onClick={() => {
+              history.push("/");
+            }}
+            variant="contained"
+            color="primary"
+            className={classes.spread}
+          >
+            Back
+          </Button>
 
-      
-            <div className={classes.cartwidth}>
-              <div className = {classes.butts}>
-                  <Button onClick = {() => {history.push('/')}} variant="contained" color="primary" className = {classes.spread}>
-                      Back
-                  </Button>
-                  
-                  <Button variant="contained" color="primary" className = {classes.spread}>
+          {props.items.length === 0 ? (
+            <Button
+              variant="contained"
+              color="primary"
+              disabled
+              className={classes.spread}
+            >
+              Checkout
+            </Button>
+          ) : (
+            <Link to="/checkout" className={classes.colorLink}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.spread}
+              >
+                Checkout
+              </Button>
+            </Link>
+          )}
+        </div>
 
-                      <Link to = "/checkout" className={classes.colorLink}>
-                        Checkout
-                      </Link>
-                      
-                  </Button>  
-              </div>   
-              
-              <TableContainer key = {Math.random()} component={Paper} className={classes.cartContainer} style={{boxshadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'}} >
-              
-                <Table key = {Math.random()} className={classes.table} aria-label="simple table" >
-                  <TableHead color="primary">
-                    <TableRow className={classes.tableHead} >
-                      <TableCell color="primary">Image</TableCell>
-                      <TableCell align="right">Name</TableCell>
-                      <TableCell align="right">Quantity</TableCell>
-                      <TableCell align="right">Price</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  {props.items.length === 0 ?
-                  <p>Cart is empty</p>
-                  :
-                  <TableBody key = {Math.random()}>
-                    {props.items.map((row) => (
-                      <TableRow key={row.name}>
-                        <TableCell component="th" scope="row">
-                          <CardMedia className={classes.CardMedia} component="img" 
-                          image={row.thumbnail_url}  title={row.name} />
-                        </TableCell>
-                        <TableCell align="right">{row.name}</TableCell>
-                        <TableCell align="right">
-                          <Button className={classes.cartQty} onClick = {() => changeQ('+', row.id)}>+</Button><span className={classes.cartQtyNo}>{row.qty}</span>
-                          <Button className={classes.cartQty} onClick = {() => changeQ('-', row.id)}>-</Button>
-                        </TableCell>
-                        <TableCell align="right">${row.price}</TableCell>
-                      </TableRow>
-                    ))}
-                    <TableRow>
-                      <TableCell></TableCell>
-                      <TableCell></TableCell>
-                      <TableCell></TableCell>
-                      <TableCell align="right"><b>Total</b>  :  ${props.total}</TableCell>
-                    </TableRow>
-                  </TableBody>
-                  }
-                </Table>
-              
-              </TableContainer>
-                      
-            </div>
-            
-      </div>  
-       
-  )
+        <TableContainer
+          key={Math.random()}
+          component={Paper}
+          className={classes.cartContainer}
+          style={{
+            boxshadow:
+              "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
+          }}
+        >
+          <Table
+            key={Math.random()}
+            className={classes.table}
+            aria-label="simple table"
+          >
+            <TableHead color="primary">
+              <TableRow className={classes.tableHead}>
+                <TableCell color="primary">Image</TableCell>
+                <TableCell align="right">Name</TableCell>
+                <TableCell align="right">Quantity</TableCell>
+                <TableCell align="right">Price</TableCell>
+              </TableRow>
+            </TableHead>
+            {props.items.length === 0 ? (
+              <p>Cart is empty</p>
+            ) : (
+              <TableBody key={Math.random()}>
+                {props.items.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      <CardMedia
+                        className={classes.CardMedia}
+                        component="img"
+                        image={row.thumbnail_url}
+                        title={row.name}
+                      />
+                    </TableCell>
+                    <TableCell align="right">{row.name}</TableCell>
+                    <TableCell align="right">
+                      <Button
+                        className={classes.cartQty}
+                        onClick={() => changeQ("+", row.id)}
+                      >
+                        +
+                      </Button>
+                      <span className={classes.cartQtyNo}>{row.qty}</span>
+                      <Button
+                        className={classes.cartQty}
+                        onClick={() => changeQ("-", row.id)}
+                      >
+                        -
+                      </Button>
+                    </TableCell>
+                    <TableCell align="right">${row.price}</TableCell>
+                  </TableRow>
+                ))}
+                <TableRow>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+                  <TableCell align="right">
+                    <b>Total</b> : ${props.total}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            )}
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
 }
